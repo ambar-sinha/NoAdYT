8cb857a0e8270ba86c91ec19167e893947a42db3
Ambar Sinha <ambarsinha06@gmail.com>
2025-07-13 11:05:43 +0530

    1.1

--- app/src/main/java/com/example/noaddyt/MainActivity.java
+++ app/src/main/java/com/example/noaddyt/MainActivity.java
@@ -23,6 +23,7 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+
 public class MainActivity extends AppCompatActivity {
 LinearLayout search, confirm, retry;
 WebView webView;
